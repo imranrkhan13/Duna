65608d007e5f979372c0d7af5f34a2d65093ce90
Imran Khan <99342205+imranrkhan13@users.noreply.github.com>
2026-07-21 22:47:02 +0530

    Delete components/marketing/TrustSection.tsx

--- components/marketing/TrustSection.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { Reveal } from "@/components/ui/Motion";
-
-const logos = ["Lexora", "Speakly", "Edvo", "Lingua", "TutorOS", "Auralab"];
-
-export function TrustSection() {
-  return (
-}
